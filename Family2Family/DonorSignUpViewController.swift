3f74dfd76d17af4058a874cc55ac859d88dfb090
ameza13 <meza.adriana86@gmail.com>
2020-05-24 12:54:47 -0700

    Basic App Flow

---
+++ Family2Family/DonorSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DonorSignUpViewController.swift
+//  Family2Family
+//
+//  Created by Adriana Meza on 5/24/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class DonorSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Family2Family/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  Family2Family
+//
+//  Created by Adriana Meza on 5/24/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        //Logic for LogIn as in Partagram example
+        
+        //Add a validation for the User type
+        
+        
+        /*self.performSegue(withIdentifier: "LogInAsDonor", sender: nil)*/
+        self.performSegue(withIdentifier: "LogInAsFamilyMember", sender: nil)
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
