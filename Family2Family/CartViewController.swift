c51a631d3359649a6addb781efa7dd2146d401a6
ameza13 <meza.adriana86@gmail.com>
2020-06-05 21:40:30 -0700

    cart view

--- Family2Family/CartViewController.swift
+++ Family2Family/CartViewController.swift
@@ -14,36 +14,43 @@ class CartViewController: UIViewController {
     var shoppingCart = [String: Int]()
     var familyData = [String: String]()
     var items = [PFObject]()
+    //var data = [String: [Any]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print(shoppingCart)
+            var idList = [String]()
+            for (key,value) in shoppingCart {
+                print("key:\(key) value:\(value)")
+                idList.append(key)
+                print(idList)
+            }
         
+            let query = PFQuery(className: "Items")
+            query.whereKey("objectId", containedIn: idList)
+            //query.whereKey("objectId", equalTo:"Ne5lGlM5e4")
+            query.findObjectsInBackground { (items, error) in
+                if items != nil{
+                    self.items = items!
+                    //print(items)
+                   
+                }else if let error = error {
+                    // Log details of the failure
+                    print(error.localizedDescription)
+                }
+            }
 
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-            let query = PFQuery(className: "Items")
-            query.whereKey("objectId", equalTo:"Ne5lGlM5e4")
-            //TO DO: Change filter to bring all items in Shopping cart in one query
-            query.findObjectsInBackground { (items, error) in
-                if items != nil{
-                    self.items = items!
-                    
-                }else if let error = error {
-                    // Log details of the failure
-                    print(error.localizedDescription)
-                }
-            }
+
         }
         //Data --> to show in Table view
         //Items: objectId, store, price, name, qty, category
         //Somehow merge with count in shoppingCart
-        
-        
+    
     func getFamily (){
         let defaults = UserDefaults.standard
         self.familyData = defaults.object(forKey: "familydata") as! [String: String]
@@ -53,16 +60,12 @@ class CartViewController: UIViewController {
         getFamily()
         print(self.familyData["username"]! as String)
         print(self.familyData["userId"]! as String)
-        
+        print(items)
         //Insert --> transactions to DB.
           //Order: family( User.objectId), total(sum price of all items in shoppingCart), fulfilled = false
           //ItemsOrder: amount(from shoppingCart), grocery (Items.objectId --> from shoppingCart, itemName (Items.name + " " + Items.store), order (Order.objectId), sellingPrice (Items.price * amount(from shoppingCart))
     }
 }
-
-    
-
-    
     
     /*
     // MARK: - Navigation
